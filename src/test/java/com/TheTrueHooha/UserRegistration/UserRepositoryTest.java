53df21d077a6a3269111b29fffabbcbb353d4460
David Ogar <owogogahhero@outlook.com>
2022-02-22 12:57:22 +0100

    test are passed

--- src/test/java/com/TheTrueHooha/UserRegistration/UserRepositoryTest.java
+++ src/test/java/com/TheTrueHooha/UserRegistration/UserRepositoryTest.java
@@ -1,9 +1,41 @@
 package com.TheTrueHooha.UserRegistration;
 
+import com.TheTrueHooha.UserRegistration.Repository.UserRepository;
+import com.TheTrueHooha.UserRegistration.User.UserParameters;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase (replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    void testCreateNewUser () {
+        UserParameters userParameters = new UserParameters();
+        userParameters.setEmail("ogun@Gmail.com");
+        userParameters.setPassword("22222222222222222222");
+        userParameters.setPhoneNumber("08025541323");
+        userParameters.setUserName("ogunboy4real");
+        userParameters.setFirstName("Ogun");
+        userParameters.setLastName("ogun");
+
+        UserParameters savedUser = userRepository.save(userParameters);
+        UserParameters existingUser = entityManager.find(UserParameters.class, savedUser.getId());
+
+        assertThat(existingUser.getEmail()).isEqualTo(userParameters.getEmail());
+
+    }
+
 }
