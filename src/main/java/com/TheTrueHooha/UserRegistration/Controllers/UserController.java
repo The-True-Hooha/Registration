715579dff0fed705bcc0248b2f22bf8a7cc7de1b
David Ogar <owogogahhero@outlook.com>
2022-02-22 15:09:09 +0100

    configuring html file

--- src/main/java/com/TheTrueHooha/UserRegistration/Controllers/UserController.java
+++ src/main/java/com/TheTrueHooha/UserRegistration/Controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.TheTrueHooha.UserRegistration.Controllers;
 
+import com.TheTrueHooha.UserRegistration.User.UserParameters;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,4 +15,9 @@ public class UserController {
         return "index";
     }
 
+    @GetMapping("/register")
+    public String registerForm (Model model){
+        model.addAttribute("user", new UserParameters());
+        return "registerForm";
+    }
 }
