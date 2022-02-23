d65bd9af07a84dd8a0a7de4b311ecb5a1dbccc0a
David Ogar <owogogahhero@outlook.com>
2022-02-23 10:18:38 +0100

    implemented UserDetails

--- src/main/java/com/TheTrueHooha/UserRegistration/Controllers/UserController.java
+++ src/main/java/com/TheTrueHooha/UserRegistration/Controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.TheTrueHooha.UserRegistration.Controllers;
 
+import com.TheTrueHooha.UserRegistration.Repository.UserRepository;
 import com.TheTrueHooha.UserRegistration.User.UserParameters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
 
     //http rest method to get data
     @GetMapping("")
@@ -19,5 +24,11 @@ public class UserController {
     public String registerForm (Model model){
         model.addAttribute("user", new UserParameters());
         return "registerForm";
+    }
+
+    @PostMapping("/success")
+    public String postRegistration(UserParameters userParameters) {
+        userRepository.save(userParameters);
+        return "success";
     }
 }
