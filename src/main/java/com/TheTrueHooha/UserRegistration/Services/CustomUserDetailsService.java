29b74981535d2a773bf23bfd714debc840f190ab
David Ogar <owogogahhero@outlook.com>
2022-02-23 10:42:42 +0100

    if statement to throw userNameNotFoundException

--- src/main/java/com/TheTrueHooha/UserRegistration/Services/CustomUserDetailsService.java
+++ src/main/java/com/TheTrueHooha/UserRegistration/Services/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.TheTrueHooha.UserRegistration.Services;
 
 import com.TheTrueHooha.UserRegistration.Repository.UserRepository;
+import com.TheTrueHooha.UserRegistration.User.CustomUserDetails;
+import com.TheTrueHooha.UserRegistration.User.UserParameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +14,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null;
+
+        //if statement to throw null if user does not exist
+        UserParameters userParameters = userRepository.findByEmail(email);
+        if (userParameters == null) {
+            throw new UsernameNotFoundException("sorry, username not found in the database");
+        }
+        return new CustomUserDetails(userParameters);
     }
 }
